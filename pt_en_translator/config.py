37eebdbe6ae1ed908d3374d5b8aa12b4c73fd542
Alexandre Lopes <alexrlops@gmail.com>
2020-06-25 03:09:17 -0300

    Add configuration yml and py

---
+++ pt_en_translator/config.py
@@ -0,0 +1,63 @@
+import os
+import yaml
+from pathlib import Path
+parent = os.path.dirname(os.path.abspath(__file__)) + '/../'
+CONFIG_FILE = parent+os.sep+"config.yml"
+
+with open(CONFIG_FILE) as f:
+    parsed_yaml_file = yaml.load(f, Loader=yaml.FullLoader)
+
+def get_source_max_lenth():
+    algs = parsed_yaml_file['source_max_length']
+    if algs is None or algs[0] is None or len(algs) == 0:
+        raise IndexError("source_max_length must not be empty in configuration file")
+
+    return algs
+
+
+def get_accumulate_batches():
+    algs = parsed_yaml_file['accumulate_grad_batches']
+    if algs is None or algs[0] is None or len(algs) == 0:
+        raise IndexError("accumulate_grad_batches must not be empty in configuration file")
+
+    return algs
+
+
+def get_target_max_length():
+    algs = parsed_yaml_file['target_max_length']
+    if algs is None or algs[0] is None or len(algs) == 0:
+        raise IndexError("source_max_length must not be empty in configuration file")
+
+    return algs
+
+
+def get_max_lenth():
+    algs = parsed_yaml_file['source_max_length']
+    if algs is None or algs[0] is None or len(algs) == 0:
+        raise IndexError("source_max_length must not be empty in configuration file")
+
+    return algs
+
+
+def get_learning_rate():
+    algs = parsed_yaml_file['learning_rate']
+    if algs is None or algs[0] is None or len(algs) == 0:
+        raise IndexError("source_max_length must not be empty in configuration file")
+
+    return algs
+
+
+def get_max_epochs():
+    algs = parsed_yaml_file['max_epochs']
+    if algs is None or algs[0] is None or len(algs) == 0:
+        raise IndexError("source_max_length must not be empty in configuration file")
+
+    return algs
+
+
+def get_model_name():
+    algs = parsed_yaml_file['model_name']
+    if algs is None or algs[0] is None or len(algs) == 0:
+        raise IndexError("source_max_length must not be empty in configuration file")
+
+    return algs
